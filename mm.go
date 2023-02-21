97b05bef4c72221bfb9c6e82469d5661f7be37d2
titi <sackotiti@gmail.com>
2023-02-21 12:46:03 +0000

    lemins

--- mm.go
+++ mm.go
@@ -8,11 +8,15 @@ import (
 	"strings"
 )
 
+type Room struct {
+	name  string
+	links []*Room
+}
+
 func main() {
 	file, err := os.ReadFile(os.Args[1])
 	if err != nil {
-		fmt.Println("Error opening file:", err)
-		os.Exit(1)
+		log.Fatal("Error opening file:", err)
 	}
 	ting := strings.Split(string(file), "\n")
 
@@ -22,14 +26,10 @@ func main() {
 	}
 	fmt.Println(antnum)
 
-	type Room struct {
-		name  string
-		links []*Room
-	}
-
-	room := make(map[string]*Room)
+	rooms := make(map[string]*Room)
 
 	var start, end *Room
+	var name string
 
 	for i, line := range ting {
 		if strings.HasPrefix(line, "##start") {
@@ -43,10 +43,28 @@ func main() {
 			end = &Room{name: name[0]}
 			continue
 		}
-		if strings.HasPrefix(line, "#") || i == 0 {
+		if strings.HasPrefix(line, "#") || i == 0 || len(line) == 0 || line[0] == 'L' {
 			continue
 		}
 
+		if len(strings.Fields(line)) == 3 {
+			name = strings.Fields(line)[0]
+			rooms[name] = &Room{name: name}
+		} else {
+			path := strings.Split(line, "-")
+
+			if rooms[path[0]].name == path[0] {
+				rooms[path[0]].links = append(rooms[path[0]].links, &Room{name: path[1]})
+			}
+		}
+
+	}
+	for start!=nil{
 		
 	}
+
+	// fmt.Println(rooms[name].links)
+	fmt.Println(start.links)
+	fmt.Println(end)
+	fmt.Println(rooms["0"].links[0] == nil)
 }
