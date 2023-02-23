b9e814189a221954a699b291a7a2f2324676d0a3
titi <sackotiti@gmail.com>
2023-02-23 16:28:13 +0000

    lemins

--- mm.go
+++ mm.go
@@ -44,6 +44,8 @@ func main() {
 		if strings.HasPrefix(line, "##end") {
 			name = strings.Fields(ting[i+1])[0]
 			end = &Room{name: name}
+
+			Rooms[name] = end
 			continue
 		}
 		if strings.HasPrefix(line, "#") || i == 0 || len(line) == 0 || line[0] == 'L' {
@@ -60,26 +62,32 @@ func main() {
 			path := strings.Split(line, "-")
 			if Rooms[path[0]].name == path[0] {
 				Rooms[path[0]].links = append(Rooms[path[0]].links, Rooms[path[1]])
+				if Rooms[path[1]].name==end.name{
+					end=Rooms[path[1]]
+				}
 			}
 		}
 
 	}
 
-	fmt.Println("ignore", end.name)
+	
 	cur := start
-	for cur.links != nil {
-		if cur == start {
-			fmt.Println("start")
-		}
+
+	fmt.Printf("start address: %p\n", start)
+	fmt.Printf("Rooms[\"0\"] address: %p\n", Rooms["0"])
+	fmt.Printf("end address: %p\n", end)
+	fmt.Printf("Rooms[\"1\"] address: %p\n", Rooms["1"])
+
+	for cur != nil {
+
 		fmt.Print(cur.name, "->")
-		if cur == end {
+		if Rooms["1"] == end {
 			fmt.Print("end")
 			break
 		}
-		if cur.links[0].links == nil {
-			fmt.Print(cur.links[0].name)
-		}
+
 		cur = cur.links[0]
 
 	}
+	fmt.Println()
 }
