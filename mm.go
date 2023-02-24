b4727af0c03b2ba1d08cf8d8796b4972eee184e8
titi <sackotiti@gmail.com>
2023-02-24 23:57:46 +0000

    lems

--- mm.go
+++ mm.go
@@ -30,7 +30,6 @@ func main() {
 
 	var start, end *Room
 	var name string
-	
 
 	for i, line := range ting {
 
@@ -59,11 +58,12 @@ func main() {
 			if name != start.name {
 				Rooms[name] = &Room{name: name}
 			}
-			
+
 		} else {
 			path := strings.Split(line, "-")
-			if Rooms[path[0]].name == path[0] {
+			if Rooms[path[0]] !=nil && Rooms[path[1]] != nil{
 				Rooms[path[0]].links = append(Rooms[path[0]].links, Rooms[path[1]])
+				Rooms[path[1]].links = append(Rooms[path[1]].links, Rooms[path[0]])
 				if Rooms[path[1]].name == end.name {
 					end = Rooms[path[1]]
 				}
@@ -73,57 +73,51 @@ func main() {
 	}
 
 	cur := start
+	fmt.Println(cur.links)
 	var Possible bool
-	for cur != nil {
-
-		fmt.Print(cur.name, "->")
-		if cur == end {
-			Possible = true
-			fmt.Print("end")
-			break
-		}
-
-		cur = cur.links[0]
-
-	}
+	//visited := make(map[*Room]bool)
+	
+	fmt.Println()
+	
 	fmt.Println()
 	fmt.Println(Possible)
+	fmt.Println(Rooms["7"])
 
 	allPaths := DFSAll(start, end)
-fmt.Println("All possible paths:")
-for _, path := range allPaths {
-    for i, room := range path {
-        fmt.Print(room.name)
-        if i < len(path)-1 {
-            fmt.Print("->")
-        }
-    }
-    fmt.Println()
+	fmt.Println("All possible paths:")
+	for _, path := range allPaths {
+		for i, room := range path {
+			fmt.Print(room.name)
+			if i < len(path)-1 {
+				fmt.Print("->")
+			}
+		}
+		fmt.Println()
+	}
 }
-	
-}
+
 func DFSAll(start, end *Room) [][]*Room {
-    visited := make(map[*Room]bool)
-    return dfsHelperAll(start, end, visited)
+	visited := make(map[*Room]bool)
+	return dfsHelperAll(start, end, visited)
 }
 
 func dfsHelperAll(curr, end *Room, visited map[*Room]bool) [][]*Room {
-    if curr == end {
-        return [][]*Room{{curr}}
-    }
+	if curr == end {
+		return [][]*Room{{curr}}
+	}
 
-    visited[curr] = true
-    var allPaths [][]*Room
+	visited[curr] = true
+	var allPaths [][]*Room
 
-    for _, neighbor := range curr.links {
-        if !visited[neighbor] {
-            neighborPaths := dfsHelperAll(neighbor, end, visited)
-            for _, path := range neighborPaths {
-                allPaths = append(allPaths, append([]*Room{curr}, path...))
-            }
-        }
-    }
+	for _, neighbor := range curr.links {
+		if !visited[neighbor] {
+			neighborPaths := dfsHelperAll(neighbor, end, visited)
+			for _, path := range neighborPaths {
+				allPaths = append(allPaths, append([]*Room{curr}, path...))
+			}
+		}
+	}
 
-    visited[curr] = false
-    return allPaths
+	visited[curr] = false
+	return allPaths
 }
