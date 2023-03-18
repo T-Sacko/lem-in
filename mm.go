81d06d4cf0fa4b98a9ac66701087ea53f952a0b6
titi <sackotiti@gmail.com>
2023-03-18 13:46:12 +0000

    yeahhhh

--- mm.go
+++ mm.go
@@ -37,7 +37,7 @@ func main() {
 			continue
 
 		}
-		
+
 		if strings.HasPrefix(line, "##end") {
 			name = strings.Fields(ting[i+1])[0]
 			end = &Room{name: name}
@@ -49,7 +49,7 @@ func main() {
 		}
 		if len(strings.Fields(line)) == 3 {
 			name = strings.Fields(line)[0]
-			if ting[i-1] == "##start"  || ting[i-1] == "##end"{
+			if ting[i-1] == "##start" || ting[i-1] == "##end" {
 				continue
 			}
 			Rooms[name] = &Room{name: name}
@@ -89,11 +89,105 @@ func main() {
 	// 		fmt.Printf("L%d-%d", ants[i], assign[i][j])
 	// 	}
 	// }
-	allPathsnew := CombinePaths(allPaths)
-	fmt.Println()
-	fmt.Println(len(allPathsnew))
-	//fmt.Println(allPathsnew[5][1][3])
-	// fmt.Println(assign[3])
+	//	allPathsnew := CombinePaths(allPaths)
+	// tingzz := ChoosePath(allPathsnew)
+	maxflow := ChoosePath(CombinePaths(allPaths))
+	queued := QueueThem(antnum, maxflow)
+	// fmt.Println()
+	// sortByLength(tingzz)
+	// fmt.Println(allPathsnew[1][0][2])
+
+	PrintResult(queued, maxflow, antnum)
+}
+
+type Ants struct {
+	Name  string
+	Path  []*Room
+	Index int
+}
+
+func PrintResult(QueuedAnts [][]string, MaxFlow [][]*Room, NumAnts int) {
+	var ants []Ants
+
+	queueCount := len(QueuedAnts)
+	var completedQueueCount int
+
+	for i := 0; NumAnts > 0; i++ {
+		for j, v := range QueuedAnts {
+			if i > len(v)-1 {
+				completedQueueCount++
+				if completedQueueCount >= queueCount {
+					break
+				}
+			} else {
+				ants = append(ants, Ants{Name: v[i], Path: MaxFlow[j], Index: 1})
+			}
+		}
+
+		for i, ant := range ants {
+			if ant.Index < len(ant.Path) {
+				vertex := ant.Path
+				fmt.Printf("%s-%s ", ant.Name, vertex[ant.Index].name)
+				ant.Index++
+				if ant.Index >= len(vertex) {
+					NumAnts--
+				}
+				ants[i] = ant
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func QueueThem(NumAnts int, MaxFlow [][]*Room) [][]string {
+	// Sort them from shortest to longest
+	sort.Slice(MaxFlow, func(i, j int) bool { return len(MaxFlow[j]) > len(MaxFlow[i]) })
+
+	// start queuing them using edmonds-karp
+	QueuedAnts := make([][]string, len(MaxFlow))
+
+	// here, we are adding all ants to the only path we have
+	// hence why len(MaxFlow) would be 1
+	if len(MaxFlow) == 1 {
+		for i := 1; i <= NumAnts; i++ {
+			AntName := "L"
+			QueuedAnts[0] = append(QueuedAnts[0], AntName)
+		}
+	} else {
+		for i := 1; i <= NumAnts; i++ {
+			AntName := "L"
+			// after adding an ant to the queue
+			// we need to decide which path does it
+			// correspond to
+			for j := 0; j < len(MaxFlow); j++ {
+				if j < len(MaxFlow)-1 {
+					PathSize1 := len(MaxFlow[j]) + len(QueuedAnts[j])
+					PathSize2 := len(MaxFlow[j+1]) + len(QueuedAnts[j+1])
+					if PathSize1 <= PathSize2 {
+						QueuedAnts[j] = append(QueuedAnts[j], AntName)
+						break
+					}
+				} else if j == len(MaxFlow)-1 {
+					QueuedAnts[j] = append(QueuedAnts[j], AntName)
+				}
+			}
+		}
+	}
+
+	// Name the ants properly
+	counter := 1
+	PathLengthCount := 0
+	for counter <= NumAnts {
+		for _, v := range QueuedAnts {
+			if len(v)-1 < PathLengthCount {
+				continue
+			}
+			v[PathLengthCount] += strconv.Itoa(counter)
+			counter++
+		}
+		PathLengthCount++
+	}
+	return QueuedAnts
 }
 
 func CombinePaths(AllPaths [][]*Room) [][][]*Room {
@@ -132,6 +226,45 @@ func CombinePaths(AllPaths [][]*Room) [][][]*Room {
 		CombPaths = nil
 	}
 	return Result
+}
+
+func ChoosePath(CombPaths [][][]*Room) [][]*Room {
+	var Max int
+	var Sum int
+	var Index int
+
+	// Start by finding the highest amount of flow
+	for i, j := 0, len(CombPaths)-1; i < j; i, j = i+1, j-1 {
+		if len(CombPaths[i]) >= len(CombPaths[j]) && Max < len(CombPaths[i]) {
+			Max = len(CombPaths[i])
+		} else if Max < len(CombPaths[j]) {
+			Max = len(CombPaths[j])
+		}
+	}
+
+	// If several paths share the same amount of flow
+	// then choose the shortest one
+	temp := 0
+	for I, P := range CombPaths {
+		if len(P) == Max {
+			for i, path := range P {
+
+				Sum += len(path)
+				if i == len(P)-1 {
+					if temp == 0 {
+						Index = I
+						temp = Sum
+					} else if Sum <= temp {
+						Index = I
+						temp = Sum
+					}
+					Sum = 0
+
+				}
+			}
+		}
+	}
+	return CombPaths[Index]
 }
 
 func inArray(s []*Room, vp *Room) (result bool) {
